27a329646bccc59afef60b0fc0839484f340850a
phonchi <steve2003121@gmail.com>
2025-07-19 12:14:45 +0800

    Update game.py

--- game.py
+++ game.py
@@ -2,32 +2,31 @@ import random
 import time
 
 def displayIntro():
-    print('''You are in a land full of dragons. In front of you,
-you see two caves. In one cave, the dragon is friendly
-and will share his treasure with you. The other dragon
-is greedy and hungry, and will eat you on sight.''')
+    print('''你來到了一個充滿龍的國度。你面前有兩個洞穴。
+在其中一個洞穴中，住著一隻友善的龍，牠願意與你分享牠的財寶。
+另一個洞穴中，住著一隻貪婪又飢餓的龍，牠會一見到你就把你吃掉。''')
     print()
 
 def chooseCave():
     cave = ''
     while cave != '1' and cave != '2':
-        print('Which cave will you go into? (1 or 2)')
+        print('你想進入哪個洞穴？（1 或 2）')
         cave = input()
 
     return cave
 
 def checkCave(chosenCave):
-    print('You approach the cave...')
+    print('你慢慢地走向洞穴...')
     time.sleep(2)
-    print('It is dark and spooky...')
+    print('裡面又黑又陰森...')
     time.sleep(2)
-    print('A large dragon jumps out in front of you! He opens his jaws and...')
+    print('一隻巨大的龍突然跳了出來！牠張開大嘴，然後...')
     print()
     time.sleep(2)
 
     friendlyCave = random.randint(1, 2)
 
     if chosenCave == str(friendlyCave):
-        print('Gives you his treasure!')
+        print('牠把財寶送給了你！')
     else:
-        print('Gobbles you down in one bite!')
+        print('牠一口就把你吞下去了！')
